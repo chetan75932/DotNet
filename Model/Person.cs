00796d33c4ddd73b8dd4e91dd29843c202e4ae78
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2023-12-21 18:49:03 +0530

    NewProjectWithWEBCommand

---
+++ Model/Person.cs
@@ -0,0 +1,16 @@
+namespace Identity;
+
+public class Person{
+    public String Name
+    {
+        get;set;
+    }
+
+    public String Business{
+        get;set;
+    }
+
+    public int Number{
+        get;set;
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,18 @@
+namespace Catalog;
+public class Product{
+    public String Prodname{
+        get;set;
+    }
+
+    public String Prodcategory{
+        get;set;
+    }
+
+    public double Prodprice{
+        get;set;
+    }
+
+    public String Imageurl{
+        get;set;
+    }
+}
