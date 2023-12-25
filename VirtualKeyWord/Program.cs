02141ad469ce9f8471d617f029081fb603bc2ef3
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2023-12-25 19:41:46 +0530

    VirtualKeyword

---
+++ VirtualKeyWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using HR;
+using Library;
+/*Employee e1=new Employee();
+e1. CalculateSalary();
+
+SalesEmployee e2=new SalesEmployee();
+e2. CalculateSalary();
+
+SalesManager e3=new SalesManager();
+e3. CalculateSalary();
+
+/*SalesEmployee e4=new SalesEmployee();
+e4. CalculateSalary();
+
+SalesManager e5=new SalesManager();
+e5. CalculateSalary();*/
+
+
+Books b=new Books();
+
+Console.WriteLine(b[0]);
+b[0]="My new Books";
+Console.WriteLine(b[0]);
+Console.WriteLine(b);
+
+
