3d63341db882b2b9c8a454e1841cdfbf1eac8c31
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2023-12-27 19:11:41 +0530

    DATABSECONNECTION-CRUD

--- DB connection/DBApp/Program.cs
+++ DB connection/DBApp/Program.cs
@@ -1,4 +1,124 @@
-﻿//dotnet add package Mysql.Data --- Command
+﻿using MySql.Data.MySqlClient;
+
+
+    //step1:-make connection
+    MySqlConnection con=new MySqlConnection();
+
+    //step2:-connecting string
+    con.ConnectionString="server=192.168.10.150;port=3306;user=dac39;password=welcome;database=dac39";
+
+
+    //step3:-writes queries
+    String query="select * from student";
+
+    //insert query
+    /*Console.WriteLine("Enter id,firstname,lastname");
+    int id=int.Parse(Console.ReadLine());
+    String namefirst=Console.ReadLine();
+    String namelast=Console.ReadLine();
+    String insertQuery="insert into student (ID,namefirst,namelast) values(@id,@namefirst,@namelast)";*/
+    /*Console.WriteLine("Enter the id to remove");
+    int id=int.Parse(Console.ReadLine());
+    String deleteQuery="delete from student where id=@id";*/
+
+    Console.WriteLine("Enter the id and updated names");
+    int id=int.Parse(Console.ReadLine());
+    String updatedName=Console.ReadLine();
+
+    String updateQuery="update student set namefirst=@updatedName where ID=@id";
+    MySqlCommand command=new MySqlCommand(updateQuery,con);
+
+    //step4:-Declare command
+    //1MySqlCommand command=new MySqlCommand(query,con);
+
+    /*2MySqlCommand command=new MySqlCommand(insertQuery,con);
+    command.Parameters.AddWithValue("@id",id);
+    command.Parameters.AddWithValue("@namefirst",namefirst);
+    command.Parameters.AddWithValue("@namelast",namelast);*/
+
+
+    /*MySqlCommand command=new MySqlCommand(deleteQuery,con);
+    command.Parameters.AddWithValue("@id",id);*/
+    command.Parameters.AddWithValue("@id",id);
+    command.Parameters.AddWithValue("@updatedName",updatedName);
+    
+    //step5:-open connection
+    try{
+        con.Open();
+        //step6:-read data object 
+        //3MySqlDataReader reader=command.ExecuteReader();
+
+         command.ExecuteNonQuery();
+
+        //read the data
+       /*4while(reader.Read())
+        {
+            int id=int.Parse(reader["ID"].ToString());
+            String first_name=reader["namefirst"].ToString();
+            String last_name=reader["namelast"].ToString();
+
+            Console.WriteLine(id+" "+first_name+" "+last_name);
+        }*/
+
+        //close reader
+       // reader.Close();
+    }catch(Exception e){
+            Console.WriteLine(e.Message);
+    }finally{
+        //connection close
+        con.Close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//dotnet add package Mysql.Data --- Command
 //use Mysql library 
 using MySql.Data.MySqlClient;
 Console.WriteLine("Welcome to the Database Connectivity.......");
@@ -49,7 +169,7 @@ try{
     reader.Close();*/
 
     //command.ExecuteNonQuery();
-   int Count= int.Parse(command.ExecuteScalar().ToString());
+   /*int Count= int.Parse(command.ExecuteScalar().ToString());
    Console.WriteLine(Count);
 }
 catch(Exception e)
@@ -60,3 +180,4 @@ finally{
     connection.Close();
 }
 
+*/
