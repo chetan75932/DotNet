17340a3780b7d105aaf51d1424450a2f7945c4ff
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2024-01-03 17:27:45 +0530

    test orm menu driven

---
+++ TestORMApp/IDBManager.cs
@@ -0,0 +1,12 @@
+namespace DAL;
+using BOL;
+
+public interface IDBManager
+{
+    List<Product> GetAll();
+    Product GetById(int id);
+    void Insert(Product prod);
+    void Delete(int id);
+    void Update(Product prod);
+    
+}
